74e2b3b538cd12519e363d548f6c40e7f5ff20c6
ama926 <amaprabhashini926@gmail.com>
2020-06-06 18:53:56 +0530

    cleared firebase creds

--- techquiz/src/config/fire.js
+++ techquiz/src/config/fire.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config ={
-    apiKey: "AIzaSyDHG-csEyaYXJj8C-8BjFIwgprE5_ekT1c",
-    authDomain: "techquiz-e2525.firebaseapp.com",
-    databaseURL: "https://techquiz-e2525.firebaseio.com",
-    projectId: "techquiz-e2525",
-    storageBucket: "techquiz-e2525.appspot.com",
-    messagingSenderId: "419845666106",
-    appId: "1:419845666106:web:e56585a8b766caefc08c03",
-    measurementId: "G-78FRVD627G"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 
 };
 
